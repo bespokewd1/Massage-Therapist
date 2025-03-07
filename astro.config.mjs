f383d3404b3a39774f9c6e17d33065bc5dc64b54
churchilldotexe <churchillexe@gmail.com>
2025-03-07 20:31:56 +0800

    feat: added react and create auto play on hero

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,18 +4,17 @@ import icon from "astro-icon";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import react from "@astrojs/react";
+
 export default defineConfig({
   // update me!
   site: "https://www.yourwebsite.com",
 
-  integrations: [
-    icon(),
-    sitemap({
-      filter: (page) => !page.includes("/admin"),
-      changefreq: "weekly",
-      priority: 0.7,
-    }),
-  ],
+  integrations: [icon(), sitemap({
+    filter: (page) => !page.includes("/admin"),
+    changefreq: "weekly",
+    priority: 0.7,
+  }), react()],
 
   vite: {
     plugins: [tailwindcss()],
