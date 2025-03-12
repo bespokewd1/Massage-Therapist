96f036b78257d86a61891857da5adf315ad8a79f
churchilldotexe <churchillexe@gmail.com>
2025-03-12 20:49:08 +0800

    chore: change config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,27 @@
-import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
-import icon from "astro-icon";
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import icon from 'astro-icon';
 
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
 
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
 export default defineConfig({
   // update me!
-  site: "https://www.yourwebsite.com",
+  site: 'https://massage-therapist.netlify.app/',
 
-  integrations: [icon(), sitemap({
-    filter: (page) => !page.includes("/admin"),
-    changefreq: "weekly",
-    priority: 0.7,
-  }), react()],
+  integrations: [
+    icon(),
+    sitemap({
+      filter: (page) => !page.includes('/admin'),
+      changefreq: 'weekly',
+      priority: 0.7,
+    }),
+    react(),
+  ],
 
   vite: {
     plugins: [tailwindcss()],
   },
 });
+
